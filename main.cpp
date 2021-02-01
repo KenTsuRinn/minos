f1ddcf724fcfa6ce1f4bda614df635873b15be26
jianyuelin <jianyuelin@westwin.com>
2021-02-01 18:56:23 +0800

    add multiple file config

--- main.cpp
+++ main.cpp
@@ -4,59 +4,65 @@
 #include <map>
 #include <inverted_item.h>
 #include <sstream>
+#include <filesystem>
 
 int main() {
-    std::map<std::string, inverted_item> indexer{};
-    std::ifstream input("jin_ping_mei_15.txt");
-    const std::string document_id{"jin_ping_mei_15.txt"};
-    int position{};
-    for (std::string line; getline(input, line);) {
-        std::u32string u32line = to_utf32(line);
+    using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+    for (const auto &dirEntry : recursive_directory_iterator("./data")) {
 
-        for (std::u32string::const_iterator it = u32line.begin(); it != u32line.end(); ++it) {
-            if (is_ignored_char(*it))
-                continue;
+        std::cout << dirEntry.path() << std::endl;
+        std::map<std::string, inverted_item> indexer{};
+        std::ifstream input(dirEntry.path());
+        const std::string document_id{dirEntry.path()};
+        int position{};
+        for (std::string line; getline(input, line);) {
+            std::u32string u32line = to_utf32(line);
 
-            ++position;
-            std::u32string ngram_token{*it, *(it + 1)};
-            std::string token = to_utf8(ngram_token);
+            for (std::u32string::const_iterator it = u32line.begin(); it != u32line.end(); ++it) {
+                if (is_ignored_char(*it))
+                    continue;
 
-            std::size_t token_hash = std::hash<std::string>{}(token);
-            std::ostringstream stringStream;
-            stringStream << document_id;
-            stringStream << ":";
-            stringStream << token;
-            std::string copyOfStr = stringStream.str();
+                ++position;
+                std::u32string ngram_token{*it, *(it + 1)};
+                std::string token = to_utf8(ngram_token);
 
-            if (indexer.contains(token)) {
-                std::map<std::string, inverted_item>::iterator it = indexer.find(token);
-                if (it != indexer.end()){
-                    if (token == "一盤") {
-                        std::cout << &(it->second) << std::endl;
+                std::size_t token_hash = std::hash<std::string>{}(token);
+                std::ostringstream stringStream;
+                stringStream << document_id;
+                stringStream << ":";
+                stringStream << token;
+                std::string copyOfStr = stringStream.str();
+
+                if (indexer.contains(token)) {
+                    std::map<std::string, inverted_item>::iterator it = indexer.find(token);
+                    if (it != indexer.end()) {
+                        if (token == "一盤") {
+                            std::cout << &(it->second) << std::endl;
+
+                        }
+                        it->second.add_position(position);
 
                     }
-                    it->second.add_position(position);
 
+
+                } else {
+                    inverted_item s{document_id, token_hash};
+                    s.add_position(position);
+                    indexer[token] = s;
                 }
-
-
-            } else {
-                inverted_item s{document_id, token_hash};
-                s.add_position(position);
-                indexer[token] = s;
             }
         }
-    }
-    for (auto const&[key, val] : indexer) {
-        if (key == "一盤"){
-            std::cout << &val<<std::endl;
-            std::cout << val.get_positions() << std::endl;
+        for (auto const&[key, val] : indexer) {
+            if (key == "一盤") {
+                std::cout << &val << std::endl;
+                std::cout << val.get_positions() << std::endl;
 
+            }
+            if (val.get_positions() > 1)
+                std::cout << key        // string (key)
+                          << ':'
+                          << std::endl;
         }
-        if (val.get_positions() > 1)
-            std::cout << key        // string (key)
-                      << ':'
-                      << std::endl;
     }
 
     return 0;
