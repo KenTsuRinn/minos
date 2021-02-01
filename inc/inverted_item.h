b1d9320086828605635bbcf8335924131c05db01
jianyuelin <jianyuelin@westwin.com>
2021-02-01 13:51:59 +0800

    update datastructure && map value change

--- inc/inverted_item.h
+++ inc/inverted_item.h
@@ -9,28 +9,31 @@
 
 class inverted_item {
 public:
-    inverted_item() : document_id{} {
+    inverted_item() = default;
 
-    }
-
-    explicit inverted_item(const std::string &id) : document_id{id} {
+    inverted_item(const std::string &doc_id, const std::size_t &token_id) : document_id{doc_id}, token_id{token_id} {
     }
 
     void add_position(int p) {
-        this->positions.emplace_back(p);
+        this->positions.push_back(p);
+    }
+
+    const std::size_t get_token_id() {
+        return this->token_id;
     }
 
     const std::string &get_document_id() {
         return this->document_id;
     }
 
-    const std::vector<int> &get_positions() {
-        return this->positions;
+    const std::vector<int>::size_type get_positions() const{
+        return this->positions.size();
     }
 
 private:
-    std::string document_id{};
-    std::vector<int> positions{};
+    std::size_t token_id;
+    std::string document_id;
+    std::vector<int> positions;
 };
 
 #endif //MINOS_INVERTED_ITEM_H
--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <utf.h>
 #include <map>
-#include <inverted_index.h>
+#include <inverted_item.h>
+#include <sstream>
 
 int main() {
-    std::map<std::string, inverted_index> indexer{};
+    std::map<std::string, inverted_item> indexer{};
     std::ifstream input("jin_ping_mei_15.txt");
     const std::string document_id{"jin_ping_mei_15.txt"};
     int position{};
@@ -19,22 +20,44 @@ int main() {
             ++position;
             std::u32string ngram_token{*it, *(it + 1)};
             std::string token = to_utf8(ngram_token);
-            std::cout << token << std::endl;
+
+            std::size_t token_hash = std::hash<std::string>{}(token);
+            std::ostringstream stringStream;
+            stringStream << document_id;
+            stringStream << ":";
+            stringStream << token;
+            std::string copyOfStr = stringStream.str();
 
             if (indexer.contains(token)) {
-                inverted_index i = indexer[token];
-                inverted_item s{document_id};
-                s.add_position(position);
-                i.add_item(s);
+                std::map<std::string, inverted_item>::iterator it = indexer.find(token);
+                if (it != indexer.end()){
+                    if (token == "一盤") {
+                        std::cout << &(it->second) << std::endl;
+
+                    }
+                    it->second.add_position(position);
+
+                }
+
+
             } else {
-                auto id = static_cast<unsigned int>(std::hash<std::string>{}(token));
-                inverted_index i{id};
-                inverted_item s{document_id};
+                inverted_item s{document_id, token_hash};
                 s.add_position(position);
-                i.add_item(s);
-                indexer[token] = i;
+                indexer[token] = s;
             }
         }
     }
+    for (auto const&[key, val] : indexer) {
+        if (key == "一盤"){
+            std::cout << &val<<std::endl;
+            std::cout << val.get_positions() << std::endl;
+
+        }
+        if (val.get_positions() > 1)
+            std::cout << key        // string (key)
+                      << ':'
+                      << std::endl;
+    }
+
     return 0;
 }
