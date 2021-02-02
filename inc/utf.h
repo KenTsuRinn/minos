763d39a56318b8c02ebd0e722e24039eb6aa2ffd
kennetsu <linsir84@gmail.com>
2021-02-03 00:13:16 +0800

    static utf op

--- inc/utf.h
+++ inc/utf.h
@@ -7,19 +7,42 @@
 #define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 
 #include <string>
+#include <locale>
+#include <codecvt>
 
-bool is_ignored_char( char32_t const & c );
-std::string to_utf8(std::string const & s);
-std::string to_utf8(std::u32string const & s);
-std::string to_utf8(std::wstring const & s);
-std::wstring to_wstring(std::wstring const & s);
-std::wstring to_wstring(std::string const & s);
-std::wstring to_wstring(std::u32string const & s);
-std::u16string to_utf16(std::string const & s);
-std::u16string to_utf16(std::u16string const & s);
-std::u32string to_utf32(std::u32string const & s);
-std::u32string to_utf32(std::string const & s);
-std::u32string to_utf32(std::wstring const & s);
-std::u32string read_with_bom(std::istream && src);
+class encoding {
+
+public:
+    static bool is_ignored_char(char32_t const &c);
+
+    static std::string to_utf8(std::string const &s);
+
+    static std::string to_utf8(std::u32string const &s);
+
+    static std::string to_utf8(std::wstring const &s);
+
+    static std::wstring to_wstring(std::wstring const &s);
+
+    static std::wstring to_wstring(std::string const &s);
+
+    static std::wstring to_wstring(std::u32string const &s);
+
+    static std::u16string to_utf16(std::string const &s);
+
+    static std::u16string to_utf16(std::u16string const &s);
+
+    static std::u32string to_utf32(std::u32string const &s);
+
+    static std::u32string to_utf32(std::string const &s);
+
+    static std::u32string to_utf32(std::wstring const &s);
+
+    static std::u32string read_with_bom(std::istream &&src);
+
+    static inline std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> char16_t_conv{};
+    static inline std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> char32_t_conv{};
+    static inline std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> wchar_conv{};
+
+};
 
 #endif //MINOS_UTF_H
